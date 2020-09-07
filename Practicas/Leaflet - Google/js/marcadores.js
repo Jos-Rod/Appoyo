df651dadd6c57523bf357b0c887bd3e1e7e24ddc
Gerardo Cabrera <gerardojaviercr@gmail.com>
2020-09-07 16:48:15 -0500

    Changes

--- Practicas/Leaflet - Google/js/marcadores.js
+++ Practicas/Leaflet - Google/js/marcadores.js
@@ -1,4 +1,5 @@
 function initMap(){
+    
     var map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 21.152639, lng: -101.711598},
         zoom: 10
@@ -18,19 +19,6 @@ function initMap(){
         icon: icono1,
         scaledSize: new google.maps.Size(25, 25),
         map: map
-    });
-
-    marker1.addListener("click", function (){
-        var informacion = new google.maps.InfoWindow;
-
-        var infoBody = '<div class="row p-0"><div class="col-12 text-center"><h5>Contenido del InfoWindow</h5></div></div>' +
-        '<div class="row p-0 pt-2">' +
-        '<div class="col-12 col-lg-2 py-2 text-center"><img width=50px src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Info_Sign.svg/512px-Info_Sign.svg.png"></div>'+
-        '<div class="col-12 col-lg-10 py-2 text-center"><p>Este es el contenido de un InfoWindow, puede agregar html en esta ventana</p></div>'+
-        '</div>'
-        informacion.setPosition(pos1);
-        informacion.setContent(infoBody);
-        informacion.open(map);
     });
 
     map.setCenter(pos1);
