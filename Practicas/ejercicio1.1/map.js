43524e8c6a112513e7389c61210842b54a09477f
Jorge Padilla <jorge@priego8a.com>
2020-09-17 19:10:00 -0500

    ejercicio 1.1

--- Practicas/ejercicio1.1/map.js
+++ Practicas/ejercicio1.1/map.js
@@ -1,3 +1,6 @@
+
+var db = firebase.firestore();
+
 const tilesProvider = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
 //http://a.tile.stamen.com/toner/${z}/${x}/${y}.png
 //https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
@@ -7,6 +10,7 @@ MapView.doubleClickZoom.disable();
 
 var quantityPoints = 0;
 var array = [];
+var contador = 0;
 
 L.tileLayer(tilesProvider, {
     maxZoom: 17,
@@ -18,14 +22,47 @@ var newIcon = L.icon({
     iconAnchor: [30, 60],
     popupAnchor: [-3, -76]
 });
+var newIcon2 = L.icon({
+    iconUrl: 'location2.png',
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [-3, -76]
+});
 
 MapView.on('dblclick', (e) => {
+    contador ++;
     let latLng = MapView.mouseEventToLatLng(e.originalEvent);
-    L.marker([latLng.lat, latLng.lng], {icon:newIcon}).addTo(MapView);
+    if (contador % 2 == 0){
+        L.marker([latLng.lat, latLng.lng], {icon:newIcon}).addTo(MapView);
+
+    }else{
+        L.marker([latLng.lat, latLng.lng], {icon:newIcon2}).addTo(MapView);
+    }
+    
+    
+
+
+    
 
     array.push(latLng);
+    
+ 
 
     if (array.length >= quantityPoints) {
+        array.forEach(element => {
+            db.collection("Puntos").add({
+                lat: element.lat,
+                lng: element.lng,
+                
+            })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+            
+        });
         var polygon = L.polygon(array).addTo(MapView);
         array = [];
     }
