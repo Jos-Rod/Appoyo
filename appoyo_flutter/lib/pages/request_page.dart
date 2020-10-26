473551dad98f8154ffcea658e96139153ec5d3dd
Jose Luis <joseluis@priego8a.com>
2020-10-25 23:27:58 -0600

    Agregados controladores para los campos de texto.

--- appoyo_flutter/lib/pages/request_page.dart
+++ appoyo_flutter/lib/pages/request_page.dart
@@ -3,6 +3,11 @@ import 'package:appoyo_flutter/widgets/map_widget.dart';
 import 'package:get/get.dart';
 
 class RequestPage extends StatelessWidget {
+
+  TextEditingController _txtProblemaTitulo = new TextEditingController();
+  TextEditingController _txtProblemaDesc = new TextEditingController();
+  TextEditingController _txtPago = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,34 +19,39 @@ class RequestPage extends StatelessWidget {
           onPressed: () => Get.back(),
         )
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text("Pedir Appoyo", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline5),
-            SizedBox(height: 30),
-            _titleInput(),
-            SizedBox(height: 20),
-            _detailInput(),
-            SizedBox(height: 20),
-            _paymentInput(context),
-            SizedBox(height: 20),
-            _mapInput()
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("Pedir Appoyo", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline5),
+              SizedBox(height: 30),
+              _titleInput(),
+              SizedBox(height: 20),
+              _detailInput(),
+              SizedBox(height: 20),
+              _paymentInput(context),
+              SizedBox(height: 20),
+              _mapInput()
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.check, color: Colors.white, size: 35),
         elevation: 20,
-        onPressed: () => Get.to(RequestPage()),
+        onPressed: () => {
+          
+        },
       ),
     );
   }
 
   Widget _titleInput(){
     return TextField(
+      controller: _txtProblemaTitulo,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.all(
@@ -56,6 +66,7 @@ class RequestPage extends StatelessWidget {
 
   Widget _detailInput(){
     return TextField(
+      controller: _txtProblemaDesc,
       maxLines: 7,
       decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -64,7 +75,7 @@ class RequestPage extends StatelessWidget {
           ),
         ),
         contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        hintText: "¿Cuál es tu problema?"
+        hintText: "Describe tu problema..."
       )
     );
   }
@@ -80,6 +91,7 @@ class RequestPage extends StatelessWidget {
         Container(
           width: 80,
           child: TextField(
+            controller: _txtPago,
             decoration: InputDecoration(
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(
